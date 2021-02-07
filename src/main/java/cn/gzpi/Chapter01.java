bb893f5097a0e9e646b935431f974542a855a0d5
HuiWang <imhwang@126.com>
2021-02-07 13:00:10 +0800

    Sedona

--- src/main/java/cn/gzpi/Chapter01.java
+++ src/main/java/cn/gzpi/Chapter01.java
@@ -1,34 +1,31 @@
-package cn.dev;
+package cn.gzpi;
 
+import org.apache.sedona.core.enums.FileDataSplitter;
+import org.apache.sedona.core.spatialRDD.PointRDD;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.datasyslab.geospark.enums.FileDataSplitter;
-import org.datasyslab.geospark.spatialRDD.PointRDD;
 
-public class Learn01 {
+class Chapter01 {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf();
-        conf.setAppName("GeoSpark01");
+        conf.setAppName("Chapter01");
         conf.setMaster("local[*]");
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-        conf.set("spark.kryo.registrator", "org.datasyslab.geospark.serde.GeoSparkKryoRegistrator");
+        conf.set("spark.kryo.registrator", "org.apache.sedona.core.serde.SedonaKryoRegistrator");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // Get SpatialRDD
-        String pointRDDInputLocation = Learn01.class.getResource("/checkin.csv").toString();
+        String pointRDDInputLocation = Chapter01.class.getResource("checkin.csv").toString();
         Integer pointRDDOffset = 0; // 地理位置(经纬度)从第0列开始
         FileDataSplitter pointRDDSplitter = FileDataSplitter.CSV;
         Boolean carryOtherAttributes = true; // 第二列的属性(酒店名)
         PointRDD rdd = new PointRDD(sc, pointRDDInputLocation, pointRDDOffset, pointRDDSplitter, carryOtherAttributes);
 
-        // 坐标系转换
-        String sourceCrsCode = "epsg:4326";
-        String targetCrsCode = "epsg:4457";
-        rdd.CRSTransform(sourceCrsCode, targetCrsCode);
+        rdd.analyze();
+        System.out.println(rdd.approximateTotalCount);
 
-        // 遍历输出
-        rdd.rawSpatialRDD.foreach(point -> {
-            System.out.println(point.toString());
+        rdd.CRSTransform("epsg:4326", "epsg:4547", true);
+        rdd.rawSpatialRDD.foreach((p)->{
+            System.out.println(p);
         });
     }
 }
